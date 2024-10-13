3398c716eaf1cfd2a6f1ec14cba6fc07ba8308ea
Raphael Matos <raphaelmsgoncalves@gmail.com>
2024-10-13 20:57:14 -0300

    add javadocs

--- src/interfaceVisual/telasStatePattern/Preview.java
+++ src/interfaceVisual/telasStatePattern/Preview.java
@@ -6,10 +6,17 @@ import modelo.mapa.Mapa;
 import javax.swing.*;
 import java.awt.*;
 
+/** É uma classe que utiliza de um {@link JFrame} e um {@link JPanel} para mostrar ao usuário
+ * um pré-visualização e um mapa de jogo selecionado.
+ */
 public class Preview {
     JFrame frame;
     JPanel panel;
 
+    /** Construtor recebe uma instância de {@link Mapa} e configura o {@link JFrame} da classe.
+     * A instância de mapa é pintada sobre o {@link JPanel} a partir da classe {@link PainelMapa}.
+     * @param mapa Um mapa do jogo.
+     */
     public Preview(Mapa mapa) {
         frame = new JFrame("Preview");
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/interfaceVisual/imagens/previewIcon.png")));
