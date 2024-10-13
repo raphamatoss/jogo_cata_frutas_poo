d867cb2301ca7248fbc89237e417d8c6f9c2f741
Vinicius Vasconi <villasboas.micska@gmail.com>
2024-10-13 02:35:18 -0300

    Adcionado o javadoc da classe fruta

--- src/modelo/entidades/Fruta.java
+++ src/modelo/entidades/Fruta.java
@@ -1,10 +1,19 @@
 package modelo.entidades;
 
 import javax.xml.namespace.QName;
-
+/**
+ * Essa classe implementa o comportamento abstrato das frutas do jogo.
+ */
 public abstract class Fruta {
 
+    /**
+     * Toda fruta tem a propriedade booleana de estar bichada ou não.
+     */
     private boolean bichada;
+
+    /**
+     * O nome da fruta é implementado internamente apenas como identidade da fruta.
+     */
     private String nome;
 
     public Fruta(boolean bichada, String nome) {
