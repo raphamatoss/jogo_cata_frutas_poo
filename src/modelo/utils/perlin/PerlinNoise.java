3f5b701da5805e9f5426229f81b7d37d7998c7bf
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-10-16 09:34:49 -0300

    finalizando ajustes do perlin noise

--- src/modelo/utils/perlin/PerlinNoise.java
+++ src/modelo/utils/perlin/PerlinNoise.java
@@ -11,154 +11,117 @@ import java.util.*;
 *
 * */
 public class PerlinNoise {
-	private static final boolean DEBUG = false;
+	private double seed;
+	private long default_size;
+	private int[] p;
+	private int[] permutation;
 
-	// Tabela de hash padrão conforme definida por Ken Perlin. Este é um array
-	// arranjado aleatoriamente com todos os números de 0 a 255, incluídos duas vezes.
-	private final static int[] permutation = {
-			151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 
-		    21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 
-		    237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 
-		    83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 
-		    216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 
-		    173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 
-		    47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 
-		    167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 
-		    251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 
-		    31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 
-		    29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180, 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 
-		    194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 
-		    197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 
-		    175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 
-		    41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 
-		    200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 
-		    38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 
-		    119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 
-		    79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 
-		    241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 
-		    50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 
-		    156, 180
-		};
-
-	public static void setSeed(long seed) {
-		// Converter a hash table para uma lista
-		ArrayList<Integer> permutationList = new ArrayList<>();
-
-		for (int p : permutation) {
-			permutationList.add(p);
-		}
-
-		Collections.shuffle(permutationList, new Random(seed));
-
-		// overwrite hash table
-		for (int i = 0; i < permutation.length; i++)
-			permutation[i] = permutationList.get(i);
+	public PerlinNoise(double seed) {
+		this.seed = seed;
+		init();
 	}
 
-    /***
-     * Função de hashing baseada na função fornecida em
-     * https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/perlin-noise-part-2,
-     * que é baseada na original de Ken Perlin.
-     * @param x coordenada x do ponto para gerar o hash
-     * @param y coordenada y do ponto para gerar o hash
-     * @return valor na faixa [0, 255] da tabela de hash
-     */
-    private static int hash(int x, int y) {
-        x = x & 255;  // mesmo que % 256, mas mais rápido
-        y = y & 255;
-        return permutation[permutation[x] + y];
-    }
-    
-    /**
-     * Para qualquer coordenada (x, y) fornecida, seleciona pseudorandomicamente um vetor de gradiente
-     * usando o método de hash.
-     * @param canto O vetor que representa as coordenadas de um canto do quadrado unitário.
-     * @return O vetor de gradiente para esse ponto, no formato {x, y}, onde
-     *         o vetor é medido com sua origem em (0, 0) e sua extremidade em
-     *         (x, y).
-     */
-    private static Vetor2D<Integer> selecionarVetorGradiente(Vetor2D<Integer> canto) {
-        // Vetores de gradiente válidos (quatro direções possíveis)
-        final int[][] vetoresGradienteValidos = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+	public PerlinNoise() {
+		this.seed = new Random().nextGaussian() * 255;
+		init();
+	}
 
-        // Retorna um vetor de gradiente com base no valor de hash(x, y) % 4 (usando bitwise & 3 para ser mais rápido)
-        int indice = hash(canto.getX(), canto.getY()) & 3;
+	private void init() {
+		// Initialize the permutation array.
+		this.p = new int[512];
+		this.permutation = new int[] { 151, 160, 137, 91, 90, 15, 131, 13, 201,
+				95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99,
+				37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26,
+				197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88,
+				237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74,
+				165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111,
+				229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40,
+				244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76,
+				132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159,
+				86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250,
+				124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207,
+				206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170,
+				213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155,
+				167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113,
+				224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242,
+				193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235,
+				249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184,
+				84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236,
+				205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66,
+				215, 61, 156, 180 };
+		this.default_size = 35;
 
-		return new Vetor2D<>(vetoresGradienteValidos[indice][0], vetoresGradienteValidos[indice][1]);
-    }
+		// Populate it
+		for (int i = 0; i < 256; i++) {
+			p[256 + i] = p[i] = permutation[i];
+		}
 
-	private static double fade(double t) {
-		// 6t^5 - 15t^4 + 10t^3
+	}
+
+	public double getSeed() {
+		return this.seed;
+	}
+
+	public double noise(double x, double y) {
+		double value = 0.0;
+		double size = default_size;
+		double initialSize = size;
+
+		while (size >= 1) {
+			value += smoothNoise((x / size), (y / size), (0f / size)) * size;
+			size /= 2.0;
+		}
+
+		return value / initialSize;
+	}
+
+	public double smoothNoise(double x, double y, double z) {
+		// Offset each coordinate by the seed value
+		x += this.seed;
+		y += this.seed;
+		x += this.seed;
+
+		int X = (int) Math.floor(x) & 255; // FIND UNIT CUBE THAT
+		int Y = (int) Math.floor(y) & 255; // CONTAINS POINT.
+		int Z = (int) Math.floor(z) & 255;
+
+		x -= Math.floor(x); // FIND RELATIVE X,Y,Z
+		y -= Math.floor(y); // OF POINT IN CUBE.
+		z -= Math.floor(z);
+
+		double u = fade(x); // COMPUTE FADE CURVES
+		double v = fade(y); // FOR EACH OF X,Y,Z.
+		double w = fade(z);
+
+		int A = p[X] + Y;
+		int AA = p[A] + Z;
+		int AB = p[A + 1] + Z; // HASH COORDINATES OF
+		int B = p[X + 1] + Y;
+		int BA = p[B] + Z;
+		int BB = p[B + 1] + Z; // THE 8 CUBE CORNERS,
+
+		return lerp(w, lerp(v, lerp(u, grad(p[AA], 		x, 		y, 		z		), 	// AND ADD
+								grad(p[BA],		x - 1, 	y, 		z		)), // BLENDED
+						lerp(u, grad(p[AB], 	x, 		y - 1, 	z		), 	// RESULTS
+								grad(p[BB], 	x - 1, 	y - 1, 	z		))),// FROM 8
+				lerp(v, lerp(u, grad(p[AA + 1], x, 		y, 		z - 1	), 	// CORNERS
+								grad(p[BA + 1], x - 1, 	y, 		z - 1	)), // OF CUBE
+						lerp(u, grad(p[AB + 1], x, 		y - 1,	z - 1	),
+								grad(p[BB + 1], x - 1, 	y - 1, 	z - 1	))));
+	}
+
+	private double fade(double t) {
 		return t * t * t * (t * (t * 6 - 15) + 10);
 	}
 
-	private static double linInt(double frac, double a, double b) {
-		return a + (frac * (b - a));
+	private double lerp(double t, double a, double b) {
+		return a + t * (b - a);
 	}
 
-	public static double perlin(double x, double y) {
-		/* ETAPA 1: Calcular o produto escalar entre o vetor de Distância e Gradiente */
-		/* CALCULANDO O QUADRADO UNITARIO DO PONTO (x, y) */
-		Map<String, Vetor2D<Integer>> quadradoUnitario = new HashMap<>();
-
-		int x0, x1, y0, y1;
-		x0 = (int) x;
-		y0 = (int) y;
-		x1 = x0 + 1;
-		y1 = y0 + 1;
-
-		quadradoUnitario.put("SuperiorEsquerdo", new Vetor2D<>(x0, y0));
-		quadradoUnitario.put("SuperiorDireito", new Vetor2D<>(x1, y0));
-		quadradoUnitario.put("InferiorEsquerdo", new Vetor2D<>(x0, y1));
-		quadradoUnitario.put("InferiorDireito", new Vetor2D<>(x0, y1));
-
-		// Produto escalar entre cada (distancia . gradiente) de cada canto.
-		Map<String, Double> produtoEscalar = new HashMap<>();
-
-		if (DEBUG) {
-			System.out.println("Ponto: " + x + ", " + y);
-		}
-
-		for (String chave : quadradoUnitario.keySet()) {
-			Vetor2D<Integer> canto = quadradoUnitario.get((chave));
-
-			Vetor2D<Double> vetorDistancia = new Vetor2D<>(x - canto.getX(), y - canto.getY());
-
-			Vetor2D<Integer> vetorGradiente = selecionarVetorGradiente(canto);
-
-			// Calculando o resultado do produto escalar entre (distancia . gradiente)
-			double resultado = vetorDistancia.getX() * vetorGradiente.getX() + vetorDistancia.getY() * vetorGradiente.getY();
-
-			produtoEscalar.put(chave, resultado);
-
-			if (DEBUG) {
-				System.out.println("Canto: " + canto.getX() + ", " + canto.getY());
-				System.out.println("Distancia: " + vetorDistancia.getX() + ", " + vetorDistancia.getY());
-				System.out.println("Gradiente: " + vetorGradiente.getX() + ", " + vetorGradiente.getY());
-				System.out.println("Produto Escalar: " + resultado);
-			}
-
-		}
-
-		/* ETAPA 2: Interpolação Bilinear de Produtos de 4 Pontos para Valor Final */
-		double dx = x - (int) x;
-		double dy = y - (int) y;
-
-		double u = fade(dx);
-		double v = fade(dy);
-
-		double superior = linInt(u, produtoEscalar.get("SuperiorEsquerdo"), produtoEscalar.get("SuperiorDireito"));
-		double inferior = linInt(u, produtoEscalar.get("InferiorEsquerdo"), produtoEscalar.get("InferiorDireito"));
-		double vert = linInt(v, superior, inferior);
-
-		if (DEBUG) {
-			System.out.println("Frac = " + u);
-			System.out.println("Superior = " + superior);
-			System.out.println("Inferior = " + inferior);
-			System.out.println("Vert = " + vert);
-		}
-
-		return (vert + 1) / 2;
+	private double grad(int hash, double x, double y, double z) {
+		int h = hash & 15; // CONVERT LO 4 BITS OF HASH CODE
+		double u = h < 8 ? x : y, // INTO 12 GRADIENT DIRECTIONS.
+				v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+		return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 	}
-
 }
--- src/modelo/utils/perlin/TestePerlin.java
+++ src/modelo/utils/perlin/TestePerlin.java
@@ -5,15 +5,27 @@ public class TestePerlin {
 		int m = 3; // Varia entre 3 a 12
 
 		// Cada bloco tem 50x50 pixels
-		int tamanhoMapa = 3 * 50;
-
-		PerlinNoise.setSeed(1);
+		int tamanhoMapa = m * 50;
 
 		double limiarFlores = 0.5;
 
+		PerlinNoise perlinNoise = new PerlinNoise();
+
+		double[][] ruidoMatriz = new double[tamanhoMapa][tamanhoMapa];
+
 		for (int i = 0; i < tamanhoMapa; i++) {
 			for (int j = 0; j < tamanhoMapa; j++) {
-				if (PerlinNoise.perlin(i * 0.1, j * 0.1) < limiarFlores)
+				double ruido = perlinNoise.noise(i, j); // [-1, 1]
+
+				double ruidoAjustado = (ruido + 1) / 2; // [0, 1]
+
+				ruidoMatriz[i][j] = ruidoAjustado;
+			}
+		}
+
+		for (int i = 0; i < tamanhoMapa; i++) {
+			for (int j = 0; j < tamanhoMapa; j++) {
+				if (ruidoMatriz[i][j] < limiarFlores)
 					System.out.print(". ");
 				else
 					System.out.print("F ");
